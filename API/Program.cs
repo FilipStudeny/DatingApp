9ba77b56fdbe6cb6cc58821b41f5f186de427b1f
Filip Studený <BluesyPompanno@seznam.cz>
2023-07-21 15:30:44 +0200

    Angular, Bootstrap and Angular over HTTPs setup

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,14 @@ builder.Services.AddDbContext<DataContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// ## CORS
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 
+// ## MIDDLEWARE
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
